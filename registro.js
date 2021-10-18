2bc823a933d30c88bf76c843eee9c1262626eeb5
laosflorez11 <laosflorez@gmail.com>
2021-10-17 19:02:44 -0500

    arreglos de obtenerUsername y FiltrarCorreo

--- registro.js
+++ registro.js
@@ -1,14 +1,38 @@
 let registros = []
 
-function obtenerUsername(arreglo) {
 
+function obtenerUsername(arreglo) { 
+
+    var arreglonombres= [];
+    var regExp = /(?=.*[0 - 9])+/gi;
+
+    for (let i = 0; i < arreglo.length; i++) {
+        
+        if(arreglo[i].nombre.match(regExp) != null){
+            arreglonombres.push(arreglo[i].nombre);
+        }    
+    }
+
+    return arreglonombres;
 
 }
+
 
 function filtrarCorreo(arreglo) {
-
-
+    var arreglocorreo=[];
+    var regExp3 = /\bhotmail.com$\b/;
+    
+    for (let i = 0; i < arreglo.length; i++) {
+    
+        if(arreglo[i].correo.match(regExp3) != null){
+            arreglocorreo.push(arreglo[i].correo);
+        }
+    
+    }
+    
+    
 }
+
 
 function agregarRegistro() {
 
